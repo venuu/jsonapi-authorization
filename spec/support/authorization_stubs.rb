2c513c44a10b77714205c15162347dd91a64212e
Emil Sågfors <emil.sagfors@iki.fi>
2016-01-27 13:42:33 +0200

    Use verifying doubles in stub helpers

--- spec/support/authorization_stubs.rb
+++ spec/support/authorization_stubs.rb
@@ -1,11 +1,11 @@
 def allow_operation(operation)
   allow(JSONAPI::Authorization::Authorizer).to receive(:new).with(any_args) do
-    double(operation => nil)
+    instance_double(JSONAPI::Authorization::Authorizer, operation => nil)
   end
 end
 
 def disallow_operation(operation)
-  raising_double = double
+  raising_double = instance_double(JSONAPI::Authorization::Authorizer)
   allow(raising_double).to receive(operation).and_raise(Pundit::NotAuthorizedError)
   allow(JSONAPI::Authorization::Authorizer).to receive(:new).with(any_args) do
     raising_double
