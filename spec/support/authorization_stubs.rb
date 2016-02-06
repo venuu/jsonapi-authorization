c0be704335c0a09ebbe3811e0440bcb535f46295
Vesa Laakso <vesa.laakso@venuu.fi>
2016-02-06 20:27:50 +0200

    Allow AuthorizationStubs to be chained

--- spec/support/authorization_stubs.rb
+++ spec/support/authorization_stubs.rb
@@ -1,18 +1,18 @@
 module AuthorizationStubs
   AUTHORIZER_CLASS = JSONAPI::Authorization::DefaultPunditAuthorizer
 
-  def allow_operation(operation, *args)
-    authorizer = instance_double(AUTHORIZER_CLASS)
+  def allow_operation(operation, *args, authorizer: instance_double(AUTHORIZER_CLASS))
     allow(authorizer).to receive(operation).with(*args).and_return(nil)
 
     allow(AUTHORIZER_CLASS).to receive(:new).with(Hash).and_return(authorizer)
+    authorizer
   end
 
-  def disallow_operation(operation, *args)
-    authorizer = instance_double(AUTHORIZER_CLASS)
+  def disallow_operation(operation, *args, authorizer: instance_double(AUTHORIZER_CLASS))
     allow(authorizer).to receive(operation).with(*args).and_raise(Pundit::NotAuthorizedError)
 
     allow(AUTHORIZER_CLASS).to receive(:new).with(Hash).and_return(authorizer)
+    authorizer
   end
 
   def allow_operations(operation, operation_args)
