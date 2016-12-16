4a17935c92b376d19b5b0fffe0d60c26db8e5e05
Matthias Grundmann <matthias@glasmail.de>
2016-12-16 12:28:56 +0100

    Fix Ruby style

--- spec/dummy/app/policies/article_policy.rb
+++ spec/dummy/app/policies/article_policy.rb
@@ -32,7 +32,7 @@ class ArticlePolicy
     raise NotImplementedError
   end
 
-  def allow_relationship_comments? comments
+  def allow_relationship_comments?(_comments)
     raise NotImplementedError
   end
 end
