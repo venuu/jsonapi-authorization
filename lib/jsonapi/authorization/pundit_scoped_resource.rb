3ff63d2cb3007522c2ea525eebb26c27de53d3b9
Bruno Sofiato <bruno.sofiato@gmail.com>
2017-02-03 12:58:20 -0200

    Fix relationships using the relation_name option (#42)
    * Enabled resolution of relationship whose name differs from the underlying model association (i.e. relationships which employ the relation_name option)
    
    * Travis warning adjustments
    
    * Created some specs

--- lib/jsonapi/authorization/pundit_scoped_resource.rb
+++ lib/jsonapi/authorization/pundit_scoped_resource.rb
@@ -14,7 +14,7 @@ module JSONAPI
 
       def records_for(association_name)
         record_or_records = @model.public_send(association_name)
-        relationship = self.class._relationships[association_name]
+        relationship = fetch_relationship(association_name)
 
         case relationship
         when JSONAPI::Relationship::ToOne
@@ -24,6 +24,19 @@ module JSONAPI
           ::Pundit.policy_scope!(user_context, record_or_records)
         else
           raise "Unknown relationship type #{relationship.inspect}"
+        end
+      end
+
+      private
+
+      def fetch_relationship(association_name)
+        relationships = self.class._relationships.select do |_k, v|
+          v.relation_name({}) == association_name
+        end
+        if relationships.empty?
+          nil
+        else
+          relationships.values.first
         end
       end
     end
--- spec/dummy/app/models/comment.rb
+++ spec/dummy/app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   has_many :tags, as: :taggable
   belongs_to :article
   belongs_to :author, class_name: 'User'
+  belongs_to :reviewing_user, class_name: 'User'
 end
