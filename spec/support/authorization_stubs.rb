257e5c642910713c9e2a90a03ba2e172e25536aa
Nicholas Rutherford <nick@medify.co.uk>
2018-06-28 17:31:12 +0100

    refactor authorization_stubs duplication with tap
    there may be a better way to combine the splat and keyword arguments in the #with call, but I can't see what it is

--- spec/support/authorization_stubs.rb
+++ spec/support/authorization_stubs.rb
@@ -2,22 +2,18 @@ module AuthorizationStubs
   AUTHORIZER_CLASS = JSONAPI::Authorization::DefaultPunditAuthorizer
 
   def allow_operation(operation, *args, authorizer: instance_double(AUTHORIZER_CLASS), **kwargs)
-    if kwargs.empty?
-      allow(authorizer).to receive(operation).with(*args).and_return(nil)
-    else
-      allow(authorizer).to receive(operation).with(*args, **kwargs).and_return(nil)
-    end
+    allow(authorizer).to receive(operation).tap {|x|
+      if kwargs.empty? then x.with(*args) else x.with(*args, **kwargs) end
+    }.and_return(nil)
 
     allow(AUTHORIZER_CLASS).to receive(:new).with(Hash).and_return(authorizer)
     authorizer
   end
 
   def disallow_operation(operation, *args, authorizer: instance_double(AUTHORIZER_CLASS), **kwargs)
-    if kwargs.empty?
-      allow(authorizer).to receive(operation).with(*args).and_raise(Pundit::NotAuthorizedError)
-    else
-      allow(authorizer).to receive(operation).with(*args, **kwargs).and_raise(Pundit::NotAuthorizedError)
-    end
+    allow(authorizer).to receive(operation).tap {|x|
+      if kwargs.empty? then x.with(*args) else x.with(*args, **kwargs) end
+    }.and_raise(Pundit::NotAuthorizedError)
 
     allow(AUTHORIZER_CLASS).to receive(:new).with(Hash).and_return(authorizer)
     authorizer
