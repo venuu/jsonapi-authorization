974d0e96696170a5e68b64dc679e2d871bc9f28b
Nicholas Rutherford <nick@medify.co.uk>
2018-06-30 01:08:40 +0100

    tests: set dummy app to use native sqlite bools
    deprecation message:
    
    ```
    DEPRECATION WARNING: Leaving `ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer`
    set to false is deprecated. SQLite databases have used 't' and 'f' to serialize
    boolean values and must have old data converted to 1 and 0 (its native boolean
    serialization) before setting this flag to true. Conversion can be accomplished
    by setting up a rake task which runs
    
      ExampleModel.where("boolean_column = 't'").update_all(boolean_column: 1)
      ExampleModel.where("boolean_column = 'f'").update_all(boolean_column: 0)
    
    for all models and all boolean columns, after which the flag must be set to
    true by adding the following to your application.rb file:
    
      Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
     (called from <top (required)> at /Users/nruth/dev/jsonapi-authorization/spec/dummy/app/models/article.rb:1)
    ```

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -25,6 +25,8 @@ class Application < Rails::Application
   # config.middleware.delete "ActionDispatch::BestStandardsSupport"
 
   config.secret_key_base = "correct-horse-battery-staple"
+
+  config.active_record.sqlite3.represent_boolean_as_integer = true
 end
 
 JSONAPI.configure do |config|
