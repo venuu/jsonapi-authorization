0369df81b5a63a0bfa7ca87c673e433713ed6498
Nicholas Rutherford <nick@medify.co.uk>
2018-06-30 01:36:05 +0100

    remove the AuthorizationStubs tap-dance
    now that positional *args have been removed from the authorizer method signatures

--- spec/support/authorization_stubs.rb
+++ spec/support/authorization_stubs.rb
@@ -1,19 +1,15 @@
 module AuthorizationStubs
   AUTHORIZER_CLASS = JSONAPI::Authorization::DefaultPunditAuthorizer
 
-  def allow_operation(operation, *args, authorizer: instance_double(AUTHORIZER_CLASS), **kwargs)
-    allow(authorizer).to receive(operation).tap {|x|
-      kwargs.empty? ? x.with(*args) : x.with(*args, **kwargs)
-    }.and_return(nil)
+  def allow_operation(operation, authorizer: instance_double(AUTHORIZER_CLASS), **kwargs)
+    allow(authorizer).to receive(operation).with(**kwargs).and_return(nil)
 
     allow(AUTHORIZER_CLASS).to receive(:new).with(context: kind_of(Hash)).and_return(authorizer)
     authorizer
   end
 
-  def disallow_operation(operation, *args, authorizer: instance_double(AUTHORIZER_CLASS), **kwargs)
-    allow(authorizer).to receive(operation).tap {|x|
-      kwargs.empty? ? x.with(*args) : x.with(*args, **kwargs)
-    }.and_raise(Pundit::NotAuthorizedError)
+  def disallow_operation(operation, authorizer: instance_double(AUTHORIZER_CLASS), **kwargs)
+    allow(authorizer).to receive(operation).with(**kwargs).and_raise(Pundit::NotAuthorizedError)
 
     allow(AUTHORIZER_CLASS).to receive(:new).with(context: kind_of(Hash)).and_return(authorizer)
     authorizer
