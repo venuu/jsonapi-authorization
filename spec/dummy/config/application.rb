e65cd2b736269f534c2f6ab662fb5481a5ad1b0e
Vesa Laakso <vesa.laakso@venuu.fi>
2019-01-07 14:19:18 +0200

    Fix deprecation warnings in tests caused by spec application config

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,8 +8,8 @@ class Application < Rails::Application
   config.cache_classes = true
 
   config.eager_load = false
-  config.serve_static_files = true
-  config.static_cache_control = "public, max-age=3600"
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
@@ -20,8 +20,8 @@ class Application < Rails::Application
 
   config.active_support.deprecation = :stderr
 
-  config.middleware.delete "Rack::Lock"
-  config.middleware.delete "ActionDispatch::Flash"
+  config.middleware.delete Rack::Lock
+  config.middleware.delete ActionDispatch::Flash
   # config.middleware.delete "ActionDispatch::BestStandardsSupport"
 
   config.secret_key_base = "correct-horse-battery-staple"
