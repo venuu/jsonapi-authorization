3abe70e619e381d850a31367c69bf2f380139a04
Vesa Laakso <vesa.laakso@venuu.fi>
2022-08-24 22:48:16 +0300

    Manually fix Naming/MethodParameterName

--- spec/dummy/app/controllers/users_controller.rb
+++ spec/dummy/app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
   end
 
   # https://github.com/cerebris/jsonapi-resources/pull/573
-  def handle_exceptions(e)
-    if JSONAPI.configuration.exception_class_whitelist.any? { |k| e.class.ancestors.include?(k) }
-      raise e
+  def handle_exceptions(err)
+    if JSONAPI.configuration.exception_class_whitelist.any? { |k| err.class.ancestors.include?(k) }
+      raise err
     end
 
     super
