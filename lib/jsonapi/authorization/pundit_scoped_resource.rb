e027fa884bc174414839000e4000d2a5b296c8f2
Vesa Laakso <vesa.laakso@venuu.fi>
2022-08-24 19:46:14 +0300

    Remove obsolete records_for method
    This method is no longer used in JR 0.10

--- lib/jsonapi/authorization/pundit_scoped_resource.rb
+++ lib/jsonapi/authorization/pundit_scoped_resource.rb
@@ -13,34 +13,6 @@ module JSONAPI
           ::Pundit.policy_scope!(user_context, super)
         end
       end
-
-      def records_for(association_name)
-        record_or_records = @model.public_send(association_name)
-        relationship = fetch_relationship(association_name)
-
-        case relationship
-        when JSONAPI::Relationship::ToOne
-          record_or_records
-        when JSONAPI::Relationship::ToMany
-          user_context = JSONAPI::Authorization.configuration.user_context(context)
-          ::Pundit.policy_scope!(user_context, record_or_records)
-        else
-          raise "Unknown relationship type #{relationship.inspect}"
-        end
-      end
-
-      private
-
-      def fetch_relationship(association_name)
-        relationships = self.class._relationships.select do |_k, v|
-          v.relation_name(context: context) == association_name
-        end
-        if relationships.empty?
-          nil
-        else
-          relationships.values.first
-        end
-      end
     end
   end
 end
